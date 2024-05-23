20efe8ff8a4e429fd8f3a6cad92dd96b3c3cca68
Bayan Rub <bayanaboalrob2001@gmail.com>
2024-05-23 12:27:25 +0300

    Implementing User Management Functionality

---
+++ AirlineBookingApp/UserManagement/AdminService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBookingApp.UserManagement
+{
+    public class AdminService
+    {
+        private UserManager _userManager;
+
+        public AdminService(UserManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public User CreateUser(string username, string password, string email, Role role)
+        {
+            return _userManager.CreateUser(username, password, email, role);
+        }
+
+        public void DeleteUser(int userId)
+        {
+            _userManager.DeleteUser(userId);
+        }
+
+        // Other administrative tasks
+        public void PerformSystemBackup()
+        {
+            // Backup logic
+        }
+
+        public void ConfigureSystemSettings(string setting, string value)
+        {
+            // Configuration logic
+        }
+    }
+}
---
+++ AirlineBookingApp/UserManagement/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBookingApp.UserManagement
+{
+    internal abstract class Manager : User
+    {
+        public string Deparment { get; set; }
+        public Manager(string username, string password, string email, Role role, string deparment) : base(username, password, email, role)
+        {
+            Deparment = deparment;
+        }
+        public abstract void GenerateReports();
+
+
+    }
+}
